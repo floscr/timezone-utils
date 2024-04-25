0cdd2df80307155e2f7153f18433d36d76b4e664
Florian Schroedl <flo.schroedl@gmail.com>
2024-04-25 19:00:00 +0200

    Highlight overlapping core times

--- src/app/components/scheduler/views.cljs
+++ src/app/components/scheduler/views.cljs
@@ -23,11 +23,12 @@
      "text-sm font-medium select-none"
      "w-12 h-6"
      {"--pill" (case kind
-                 :muted (gstring/format "oklch(80.93% 0.02 %s)" hue)
-                 (gstring/format "oklch(80.93% 0.099 %s)" hue))
+                 :active (gstring/format "oklch(85% 0.09 %s)" hue)
+                 :muted (gstring/format "oklch(85% 0.01 %s)" hue)
+                 (gstring/format "oklch(81% 0.13 %s)" hue))
       :background "var(--pill)"
       :color "oklch(from var(--pill) calc(l - 0.4) c h)"
-      "&:hover" {:background "oklch(from var(--pill) calc(l + 0.1) c h)"}})
+      "&:hover" {:background "oklch(from var(--pill) calc(l + 0.08) c h)"}})
 
 ;; Helpers ---------------------------------------------------------------------
 
@@ -48,21 +49,37 @@
     (and (>= h 8)
          (<= h 18))))
 
-(defui time-slots [{:keys [hue times]}]
+(defn logical-intersect [colls]
+  (apply map (fn [& vals] (every? true? vals)) colls))
+
+(defui time-slots [{:keys [hue times overlaps]}]
   ($ :ol {:class (timeslot-list-css)}
-     (for [time times]
+     (for [[time overlaps?] (map vector times overlaps)]
        ($ :li {:class (timeslot-item-css
                        hue
-                       (if (work-hour? time) :active :muted))
+                       (cond
+                         overlaps? :overlapping
+                         (work-hour? time) :active
+                         :else :muted))
                :key (str time)}
           ($ :span (t/format (t/formatter "HH:mm") time))))))
+
+(defn work-hours-intersections
+  "Returns a list of booleans that will mark every overlapping working hours as true."
+  [times]
+  (->> times
+       (map (fn [hours] (map work-hour? hours)))
+       (logical-intersect)))
 
 (defui root [{:keys []}]
   (let [my-timezones (day-hours "GMT+2")
         ist-timezones (->> (day-hours "UTC+02:00")
-                           (map #(t/in % "UTC+05:30")))]
+                           (map #(t/in % "UTC+05:30")))
+        overlaps (work-hours-intersections [my-timezones ist-timezones])]
     ($ :div {:class (wrapper-css)}
        ($ time-slots {:times my-timezones
-                      :hue "78.11"})
+                      :hue "78.11"
+                      :overlaps overlaps})
        ($ time-slots {:times ist-timezones
-                      :hue "120.11"}))))
+                      :hue "120.11"
+                      :overlaps overlaps}))))
